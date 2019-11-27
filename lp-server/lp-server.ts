885ac708438f436a6191c44e05cd20bc7eaa2446
Eidson <ejas@cin.ufpe.br>
2019-11-26 21:26:19 -0300

    transformacao de estudoscomparativos em servico

--- lp-server/lp-server.ts
+++ lp-server/lp-server.ts
@@ -3,7 +3,13 @@ import bodyParser = require("body-parser");
 
 // add imports here
 
+import {EstudosComparativos} from '../common/estudoscomparativos';
+import { CadastroDePesquisadores } from './cadastrodepesquisadores';
+import { Pesquisador } from '../common/pesquisador';
+
 var lpserver = express();
+let cadatroPesq = new CadastroDePesquisadores();
+
 
 // add services here
 
@@ -19,6 +25,66 @@ lpserver.use(allowCrossDomain);
 lpserver.use(bodyParser.json());
 
 // add reqs here
+
+lpserver.get('/estudos-comparativos', (req: express.Request, res: express.Response) => {
+  //let pesquisadores: Pesquisador[] = cadastroPesq.getPesquisadores();
+  let pesquisadores: any = [{"nome":"Paulo Henrique Monteiro Borba","orgao":"","publicacoes":[{"titulo":"States as Specifications","periodico":"I Simp�sio Brasileiro de Linguagens de Programa��o (SBLP 1996)"},{"titulo":"An Operational Semantics for FOOPS","periodico":"Information Systems Correctness And Reusability (ISCORE 1994)"}]}, {"nome":"Eidson Jaco","orgao":"","publicacoes":[{"titulo":"States as Specifications","periodico":"I Simp�sio Brasileiro de Linguagens de Programa��o (SBLP 1996)"},{"titulo":"An Operational Semantics for FOOPS","periodico":"Information Systems Correctness And Reusability (ISCORE 1994)"}]}];
+  let pesos: number[] = [1, 1, 1, 1, 1, 1, 1, 1];
+  let ranking: any = [];
+
+  pesquisadores.forEach((pesq: any) => {
+    let sumPont = 0;
+    pesq.publicacoes.forEach((publi: any) => {
+      let currentPont = 0
+      //let nota = Qualis.getAvaliacao(publi.periodico)
+      let nota;
+      // inicio codigo imbecil para teste
+        if (publi.periodico == "I Simp�sio Brasileiro de Linguagens de Programa��o (SBLP 1996)"){
+          nota = 'A2'
+        }
+        else if (publi.periodico == "Information Systems Correctness And Reusability (ISCORE 1994)"){
+          nota = 'A1'
+        }
+        else if (publi.periodico == "I Simp�sio Brasileiro de Linguagens de Programa��o (SBLP 1996)"){
+          nota = 'C'
+        }
+        else if (publi.periodico == "Information Systems Correctness And Reusability (ISCORE 1994)"){
+          nota = 'B2'
+        }
+      // fim codigo imbecil para teste
+
+      if (nota == 'A1'){
+        currentPont = pesos[0];
+      } else if (nota == 'A2') {
+        currentPont = pesos[1];
+      } else if (nota == 'B1') {
+        currentPont = pesos[2];
+      } else if (nota == 'B2') {
+        currentPont = pesos[3];
+      } else if (nota == 'B3') {
+        currentPont = pesos[4];
+      } else if (nota == 'B4') {
+        currentPont = pesos[5];
+      } else if (nota == 'B5') {
+        currentPont = pesos[6];
+      } else if (nota == 'C') {
+        currentPont = pesos[7];
+      } else {
+        currentPont = 0;
+      }
+      sumPont += currentPont;
+    });
+    
+    ranking.push({
+        pesquisador: pesq,
+        pontos: sumPont,
+    })
+
+  });
+  ranking.sort((a: any, b: any) => (a.pontos > b.pontos) ? 1 : (a.pontos == b.pontos) ? ((a.pesquisador.nome > b.pesquisador.nome) ? 1 : -1) : -1)
+
+  res.send(ranking)
+})
 
 var server = lpserver.listen(3000, function () {
   console.log('Example app listening on port 3000!')
